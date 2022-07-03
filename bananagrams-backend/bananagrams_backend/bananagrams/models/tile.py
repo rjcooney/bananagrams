96a1e9eacd948eb4c4c81c46032cbe1bbf2d7fe5
Ryan Cooney <rjcooney@wpi.edu>
2022-07-03 10:14:38 -0400

    Update tile model

--- bananagrams-backend/bananagrams_backend/bananagrams/models/tile.py
+++ bananagrams-backend/bananagrams_backend/bananagrams/models/tile.py
@@ -1,12 +1,17 @@
 from msilib.schema import Class
 from django.db import models
+from bananagrams_backend.bananagrams.models.board import Board
+
+from bananagrams_backend.bananagrams.models.game import Game
 
 # TODO: my thought right now is that we'll have a list of "tiles" associated with a specific game
 # so that we can track the exact tiles within a given game. Not sure that's necessary at all
 # This means we'd have several tile instances for the same letter within a game
 class Tile(models.Model):
     letter = models.CharField(max_length=1)
+    game = models.ForeignKey(Game)
 
+    board = models.ForeignKey(Board)
 
     def __str__(self) -> str:
         return self.id
